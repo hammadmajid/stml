971bd6fe99ab157f7bbf8722b9f41ec5442c8bac
Hammad Majid <hammadmajid@proton.me>
2026-01-04 19:38:49 +0500

    feat: create token module with Token type

---
+++ src/token/mod.rs
@@ -0,0 +1,49 @@
+pub mod scanner;
+
+#[derive(Debug)]
+pub enum Token {
+    // Structural keywords
+    Doc,
+    Meta,
+    Section,
+
+    // Block elements
+    P,
+    Ul,
+    Ol,
+    Li,
+
+    // Inline elements
+    Link,
+    Ref,
+
+    // Resource and linking keywords
+    Use,
+    As,
+    To,
+
+    // Media related keywords
+    File,
+    Image,
+    Src,
+    Alt,
+    Fallback,
+
+    // Content and control keywords
+    Text,
+    Maybe,
+
+    // Literals
+    Identifier,
+    StringLiteral,
+
+    // Multi-character tokens
+    TripleQuote,
+
+    // Single-character tokens
+    LeftBrace,
+    RightBrace,
+    Hash,
+    Dot,
+    Colon,
+}
