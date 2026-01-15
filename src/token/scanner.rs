1d3a407e93a85eb6fc419f14fe9141d7a069854e
Hammad Majid <hammadmajid@proton.me>
2026-01-16 01:39:13 +0500

    feat: implement the scanner

--- src/token/scanner.rs
+++ src/token/scanner.rs
@@ -1,30 +1,105 @@
 use crate::token::Token;
+use std::iter::Peekable;
+use std::str::Chars;
 
 #[derive(Debug)]
 pub enum ScannerError {
     InvalidToken(char),
     UnterminatedToken(char),
-    UnexpectedEOF,
 }
 
-pub struct Scanner {
-    string: String,
-    pos: usize,
+pub struct Scanner {}
+
+impl Scanner {
+    pub fn new() -> Scanner {
+        Scanner {}
+    }
+
+    pub fn tokenize(self, input: &str) -> Result<Vec<Token>, ScannerError> {
+        let mut tokens: Vec<Token> = Vec::new();
+        let mut chars = input.chars().peekable();
+
+        while let Some(ch) = chars.next() {
+            match ch {
+                ' ' | '\t' | '\n' => {}
+
+                '{' => tokens.push(Token::LeftBrace),
+                '}' => tokens.push(Token::RightBrace),
+                '#' => tokens.push(Token::Hash),
+                '.' => tokens.push(Token::Dot),
+                ':' => tokens.push(Token::Colon),
+
+                '"' => {
+                    let token = Self::consume_string_literal(&mut chars)?;
+                    tokens.push(token);
+                }
+
+                'a'..='z' => {
+                    let token = Self::consume_keyword_or_literal(ch, &mut chars)?;
+                    tokens.push(token);
+                }
+
+                _ => return Err(ScannerError::InvalidToken(ch)),
+            }
+        }
+
+        Ok(tokens)
+    }
 }
 
 impl Scanner {
-    pub fn new(input: String) -> Scanner {
-        Scanner {
-            string: input,
-            pos: 0,
+    fn consume_string_literal(chars: &mut Peekable<Chars>) -> Result<Token, ScannerError> {
+        let mut value = String::new();
+
+        while let Some(ch) = chars.peek() {
+            if *ch == '"' {
+                chars.next(); // discard the closing quote
+                return Ok(Token::StringLiteral(value));
+            }
+            value.push(*ch);
+            chars.next();
         }
+
+        Err(ScannerError::UnterminatedToken('"'))
     }
 
-    pub fn tokenize(&mut self) -> Result<Vec<Token>, ScannerError> {
-        let mut tokens: Vec<Token> = Vec::new();
+    fn consume_keyword_or_literal(
+        first: char,
+        chars: &mut Peekable<Chars>,
+    ) -> Result<Token, ScannerError> {
+        let mut value = String::new();
+        value.push(first);
 
-        todo!();
+        while let Some(ch) = chars.peek() {
+            if ch.is_ascii_lowercase() {
+                value.push(*ch);
+                chars.next();
+            } else {
+                return Err(ScannerError::InvalidToken('\''));
+            }
+        }
 
-        Ok(tokens)
+        match value.as_str() {
+            "doc" => Ok(Token::Doc),
+            "p" => Ok(Token::P),
+            "image" => Ok(Token::Image),
+            "section" => Ok(Token::Section),
+            "fallback" => Ok(Token::Fallback),
+            "maybe" => Ok(Token::Maybe),
+            "src" => Ok(Token::Src),
+            "alt" => Ok(Token::Alt),
+            "link" => Ok(Token::Link),
+            "to" => Ok(Token::To),
+            "text" => Ok(Token::Text),
+            "ul" => Ok(Token::Ul),
+            "li" => Ok(Token::Li),
+            "ol" => Ok(Token::Ol),
+            "ref" => Ok(Token::Ref),
+            "use" => Ok(Token::Use),
+            "as" => Ok(Token::As),
+            "file" => Ok(Token::File),
+            "meta" => Ok(Token::Meta),
+            _ => Ok(Token::Identifier(value)),
+        }
     }
 }
